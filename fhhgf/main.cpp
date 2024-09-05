df42496f49d8f49235a1c83bdcc0bd48edc0a0c4
ogfefv1 <itstepstudentdzhem06@gmail.com>
2024-09-05 23:31:37 +0300

    Initial Commit

---
+++ fhhgf/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fhhgf
+//
+//  Created by Егор Джемлиханов on 05.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
